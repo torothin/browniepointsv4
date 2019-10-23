92d223f5eed7c2a41e88b75f67f61d9d5c3495bf
Brad Duvall <bradford.duvall@gmail.com>
2019-10-22 21:25:39 -0700

    Updates

--- precache-manifest.133dd0748c6a657b005db046d62b6963.js
+++ precache-manifest.133dd0748c6a657b005db046d62b6963.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "369b8c70789977cc0ebd95d6c75798c6",
+    "revision": "57f06b6e130dac7021dbe7f7e794ba4c",
     "url": "/browniepointsv4/index.html"
   },
   {
-    "revision": "79882bb832b01976fa76",
-    "url": "/browniepointsv4/static/css/main.030b3aba.chunk.css"
+    "revision": "bb52c6f6c4b4c2bd85cd",
+    "url": "/browniepointsv4/static/css/main.a5d5cbd0.chunk.css"
   },
   {
     "revision": "7560f6a91a7b0f3f3b9f",
     "url": "/browniepointsv4/static/js/2.2d97f4f6.chunk.js"
   },
   {
-    "revision": "79882bb832b01976fa76",
-    "url": "/browniepointsv4/static/js/main.13c1d448.chunk.js"
+    "revision": "bb52c6f6c4b4c2bd85cd",
+    "url": "/browniepointsv4/static/js/main.d054f0a3.chunk.js"
   },
   {
     "revision": "a9281abd1fb4c896b91f",
