8539e257c32974e955035a26fce41815fedf4258
Brad Duvall <bradford.duvall@gmail.com>
2019-10-22 19:49:14 -0700

    Updates

--- precache-manifest.abcbe2f4ec43077930bae0f8f212ac7f.js
+++ precache-manifest.abcbe2f4ec43077930bae0f8f212ac7f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b24e69ea0a0fad13afb0da0724e59cda",
+    "revision": "f6db39ff801b9596e6bb061ab5db9b56",
     "url": "/browniepointsv4/index.html"
   },
   {
-    "revision": "0f3ff670c07b07166f48",
-    "url": "/browniepointsv4/static/css/main.976004a4.chunk.css"
+    "revision": "7a0c83b240a21e3ccf19",
+    "url": "/browniepointsv4/static/css/main.030b3aba.chunk.css"
   },
   {
     "revision": "7560f6a91a7b0f3f3b9f",
     "url": "/browniepointsv4/static/js/2.2d97f4f6.chunk.js"
   },
   {
-    "revision": "0f3ff670c07b07166f48",
-    "url": "/browniepointsv4/static/js/main.1ccb044d.chunk.js"
+    "revision": "7a0c83b240a21e3ccf19",
+    "url": "/browniepointsv4/static/js/main.898c9f78.chunk.js"
   },
   {
     "revision": "a9281abd1fb4c896b91f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/browniepointsv4/precache-manifest.80e348b7d0aebde25f0f910ddb4bec14.js"
+  "/browniepointsv4/precache-manifest.abcbe2f4ec43077930bae0f8f212ac7f.js"
 );
 
 self.addEventListener('message', (event) => {
